186428f12c0a4521f4f2c23a47e33c8c91bf4a28
AmmarJamil23 <ammarjamil7000@gmail.com>
2025-11-17 16:41:24 +0500

    created a model and created and used superuser

--- PROJECT1/base/views.py
+++ PROJECT1/base/views.py
@@ -3,10 +3,15 @@ from django.shortcuts import render
 
 
 # Create your views here.
-
+rooms = [
+    {'id': 2, 'name': 'Lets learn python'},
+    {'id': 3, 'name': 'Frontend development'},
+    {'id': 4, 'name': 'Backend development'},
+]
 
 def home(request):
-    return render(request, 'home.html')
+    context = {'rooms': rooms}
+    return render(request, 'home.html', context)
 
 def room(request):
     return render(request, 'room.html')
